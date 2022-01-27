e92f5f1fbf6b6fdda499cba785ddd79dc1209a9e
Daniel Hauser <36034483+daniel-hauser@users.noreply.github.com>
2022-01-27 20:50:12 +0000

    Better env vars

--- src/config.ts
+++ src/config.ts
@@ -13,18 +13,27 @@ const {
   ACCOUNTS_TO_SCRAPE = "",
 } = process.env;
 
+/**
+ * Add default values in case the value is falsy (0 is not valid here) or an empty string
+ */
+const daysBackToScrape = DAYS_BACK || 10;
+const worksheetName = WORKSHEET_NAME || "_no_name";
+
 const accountsToScrape = ACCOUNTS_TO_SCRAPE.split(",")
   .map((a) => a.trim())
   .filter(Boolean);
 
-export { TELEGRAM_API_KEY, TELEGRAM_CHAT_ID, GOOGLE_SHEET_ID, WORKSHEET_NAME };
+export {
+  TELEGRAM_API_KEY,
+  TELEGRAM_CHAT_ID,
+  GOOGLE_SHEET_ID,
+  worksheetName as WORKSHEET_NAME,
+};
 export const systemName = "moneyman";
 export const currentDate = format(Date.now(), "yyyy-MM-dd");
-export const scrapeStartDate = subDays(Date.now(), Number(DAYS_BACK));
+export const scrapeStartDate = subDays(Date.now(), Number(daysBackToScrape));
 
-export const accounts = (
-  JSON.parse(ACCOUNTS_JSON) as Array<AccountConfig>
-).filter(
+export const accounts = parseAccounts(ACCOUNTS_JSON).filter(
   (account) =>
     accountsToScrape.length == 0 || accountsToScrape.includes(account.companyId)
 );
@@ -41,3 +50,15 @@ export const FileHeaders = [
   "scraped at",
   "scraped by",
 ];
+
+function parseAccounts(accountsJson: string): Array<AccountConfig> {
+  try {
+    const parsed = JSON.parse(accountsJson);
+    if (Array.isArray(parsed)) {
+      // TODO: Add schema validations?
+      return parsed as Array<AccountConfig>;
+    }
+  } catch {}
+
+  throw new TypeError("ACCOUNTS_JSON must be a valid array");
+}
