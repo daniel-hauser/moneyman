df35b4dd1c7a93e1bf602afedb4ff6ae88bd700f
gczobel <754466+gczobel@users.noreply.github.com>
2024-01-12 16:43:55 +0200

    fix: added grace to the fails

--- src/storage/ynab.ts
+++ src/storage/ynab.ts
@@ -6,13 +6,18 @@ import * as ynab from "ynab";
 import hash from "hash-it";
 
 const YNAB_DATE_FORMAT = "yyyy-MM-dd";
-let ynabAPI: ynab.API;
-
 const logger = createLogger("YNABStorage");
 
 export class YNABStorage implements TransactionStorage {
+  private ynabAPI: ynab.API;
+  private budgetName: string;
+  private accountToYnabAccount: Map<string, string>;
+
   async init() {
     logger("init");
+    this.ynabAPI = new ynab.API(YNAB_TOKEN);
+    this.budgetName = await this.getBudgetName(YNAB_BUDGET_ID);
+    this.accountToYnabAccount = this.parseYnabAccounts(YNAB_ACCOUNTS);
   }
 
   canSave() {
@@ -22,82 +27,94 @@ export class YNABStorage implements TransactionStorage {
   async saveTransactions(txns: Array<TransactionRow>) {
     await this.init();
 
-    ynabAPI = new ynab.API(YNAB_TOKEN);
-    const budgetName = await getBudgetName(YNAB_BUDGET_ID);
+    // Converting to YNAB format.
+    const transactionsFromFinancialAccount = txns.map((tx) =>
+      this.convertTransactionToYnabFormat(tx),
+    );
 
-    // Convert transactions to YNAB format
-    logger("transforming transactions to ynab format");
-    const transactionsFromFinancialAccount = txns.map(
-      convertTransactionToYnabFormat,
+    // Filter out transactions with no account number
+    const transactionsWithAccount = transactionsFromFinancialAccount.filter(
+      (tx) => tx.account_id !== "",
     );
 
     // Send transactions to YNAB
-    logger(`sending to YNAB budget: "${budgetName}"`);
-    await ynabAPI.transactions.createTransactions(YNAB_BUDGET_ID, {
-      transactions: transactionsFromFinancialAccount,
-    });
+    logger(`sending to YNAB budget: "${this.budgetName}"`);
+    const resp = await this.ynabAPI.transactions.createTransactions(
+      YNAB_BUDGET_ID,
+      {
+        transactions: transactionsWithAccount,
+      },
+    );
     logger("transactions sent to YNAB successfully!");
+
+    const dups = resp.data.duplicate_import_ids
+      ? resp.data.duplicate_import_ids.length
+      : 0;
+    const noID = txns.length - transactionsWithAccount.length;
 
     const stats: SaveStats = {
       name: "YNABStorage",
-      table: `budget: "${budgetName}"`,
-      total: txns.length,
-      added: txns.length,
+      table: `budget: "${this.budgetName}"`,
+      total: transactionsFromFinancialAccount.length,
+      added: resp.data.transactions ? resp.data.transactions.length : 0,
       pending: NaN,
-      skipped: 0,
-      existing: NaN,
+      skipped: noID + dups,
+      existing: dups,
     };
 
     return stats;
   }
-}
 
-function convertTransactionToYnabFormat(
-  tx: TransactionRow,
-): ynab.SaveTransaction {
-  const amount = Math.round(tx.chargedAmount * 1000);
-
-  return {
-    account_id: getYnabAccountIdByAccountNumberFromTransaction(tx.account),
-    date: format(parseISO(tx.date), YNAB_DATE_FORMAT, {}),
-    amount,
-    payee_id: null,
-    payee_name: tx.description,
-    cleared: ynab.TransactionClearedStatus.Cleared,
-    approved: false,
-    import_id: hash(tx.hash).toString(),
-    memo: tx.memo,
-  };
-}
-
-function getYnabAccountIdByAccountNumberFromTransaction(
-  transactionAccountNumber: string,
-): string {
-  let jsonData: any;
-  try {
-    jsonData = JSON.parse(YNAB_ACCOUNTS);
-  } catch (parseError) {
-    const customError = new Error(
-      `Error parsing JSON in YNAB_ACCOUNTS ': ${parseError.message}`,
-    );
-    throw customError;
+  private async getBudgetName(budgetId: string) {
+    const budgetResponse = await this.ynabAPI.budgets.getBudgetById(budgetId);
+    if (budgetResponse.data) {
+      return budgetResponse.data.budget.name;
+    } else {
+      throw new Error(`YNAB_BUDGET_ID does not exist in YNAB: ${budgetId}`);
+    }
   }
 
-  const ynabAccountId = jsonData[transactionAccountNumber];
-  if (!ynabAccountId) {
-    throw new Error(
-      `Cannot found YNAB account UUID for account number ${transactionAccountNumber}`,
-    );
-  }
-  return ynabAccountId;
-}
+  private parseYnabAccounts(accountsJSON: string): Map<string, string> {
+    let jsonData: any;
+    try {
+      jsonData = JSON.parse(accountsJSON);
+    } catch (parseError) {
+      const customError = new Error(
+        `Error parsing JSON in YNAB_ACCOUNTS: ${parseError.message}`,
+      );
+      throw customError;
+    }
 
-async function getBudgetName(budgetId: string) {
-  const budgetResponse = await ynabAPI.budgets.getBudgetById(budgetId);
-  if (budgetResponse.data) {
-    const budgetName = budgetResponse.data.budget.name;
-    return budgetName;
-  } else {
-    throw new Error(`YNAB_BUDGET_ID does not exists in YNAB ${budgetId}`);
+    return new Map(Object.entries(jsonData));
+  }
+
+  private convertTransactionToYnabFormat(
+    tx: TransactionRow,
+  ): ynab.SaveTransaction {
+    const amount = Math.round(tx.chargedAmount * 1000);
+    const accountId = this.getYnabAccountIdByAccountNumberFromTransaction(
+      tx.account,
+    );
+
+    return {
+      account_id: accountId === null ? "" : accountId,
+      date: format(parseISO(tx.date), YNAB_DATE_FORMAT, {}),
+      amount,
+      payee_id: null,
+      payee_name: tx.description,
+      cleared: ynab.TransactionClearedStatus.Cleared,
+      approved: false,
+      import_id: hash(tx.hash).toString(),
+      memo: tx.memo,
+    };
+  }
+
+  private getYnabAccountIdByAccountNumberFromTransaction(
+    transactionAccountNumber: string,
+  ): string | null {
+    const ynabAccountId = this.accountToYnabAccount.get(
+      transactionAccountNumber,
+    );
+    return ynabAccountId || null;
   }
 }
