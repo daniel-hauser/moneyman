f8ac8f903c4056756f1edf748e701d97ee09c6d8
Daniel Hauser <36034483+daniel-hauser@users.noreply.github.com>
2024-06-25 05:59:41 +0300

    feat: Add charged currency field to Google Sheets storage (#289)

---
+++ src/utils/currency.test.ts
@@ -0,0 +1,31 @@
+import { normalizeCurrency } from "./currency";
+
+describe("normalizeCurrency", () => {
+  it("should return undefined if currency is undefined", () => {
+    const result = normalizeCurrency(undefined);
+    expect(result).toBeUndefined();
+  });
+
+  it.each([
+    ["$", "USD"],
+    ["€", "EUR"],
+    ["₪", "ILS"],
+    ["usd", "USD"],
+  ])(
+    "should return the normalized currency for %s symbol as %s",
+    (currency, expected) => {
+      const result = normalizeCurrency(currency);
+      expect(result).toBe(expected);
+    },
+  );
+
+  it("should return the currency as is if it is not a known symbol", () => {
+    const result = normalizeCurrency("GBP");
+    expect(result).toBe("GBP");
+  });
+
+  it("should normalize the currency to uppercase", () => {
+    const result = normalizeCurrency("usd");
+    expect(result).toBe("USD");
+  });
+});
