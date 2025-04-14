6636271bc2bf7e14f13eda2f9a722d9743daeb05
Daniel Hauser <36034483+daniel-hauser@users.noreply.github.com>
2025-04-14 23:46:34 +0300

    feat: nicer reports (#436)
    * fix: add reporters configuration to jest config
    
    * fix: enhance domain tracking logic with resource types
    
    * fix: update domain tracking logic and snapshot tests for resource types

--- src/security/domains.ts
+++ src/security/domains.ts
@@ -7,10 +7,13 @@ import { addToKeyedSet } from "../utils/collections.js";
 
 const logger = createLogger("domain-security");
 
+type CompanyToSet = Map<CompanyTypes, Set<string>>;
+
 const domainsFromNode: Set<string> = new Set();
-const pagesByCompany: Map<CompanyTypes, Set<string>> = new Map();
-const blockedByCompany: Map<CompanyTypes, Set<string>> = new Map();
-const allowedByCompany: Map<CompanyTypes, Set<string>> = new Map();
+const pagesByCompany: CompanyToSet = new Map();
+const blockedByCompany: CompanyToSet = new Map();
+const allowedByCompany: CompanyToSet = new Map();
+const resourceTypesByCompany: Map<string, CompanyToSet> = new Map();
 
 export function monitorNodeConnections() {
   if (process.env.DOMAIN_TRACKING_ENABLED) {
@@ -57,14 +60,25 @@ export async function initDomainTracking(
               const url = new URL(request.url());
               const pageUrl = new URL(page.url());
 
-              const reqKey = `${request.method()}(${request.resourceType()}) ${url.hostname}`;
+              const resourceType = request.resourceType();
+              const reqKey = `${request.method()} ${url.hostname}`;
+
               if (request.isInterceptResolutionHandled()) {
-                logger(`[${companyId}] Request already handled ${reqKey}`);
-                logToMetadataFile(
-                  `[${companyId}] Request already handled ${reqKey}`,
-                );
+                const message = `[${companyId}] Request already handled ${reqKey} ${resourceType}`;
+                logger(message);
+                logToMetadataFile(message);
                 return;
               }
+
+              if (!resourceTypesByCompany.has(reqKey)) {
+                resourceTypesByCompany.set(reqKey, new Map());
+              }
+
+              addToKeyedSet(
+                resourceTypesByCompany.get(reqKey)!,
+                companyId,
+                resourceType,
+              );
 
               if (ignoreUrl(url.hostname) || !rules.isBlocked(url, companyId)) {
                 addToKeyedSet(allowedByCompany, companyId, reqKey);
@@ -119,14 +133,22 @@ export async function getUsedDomains(): Promise<
 
   logger(`Reporting used domains`, { allowedByCompany, blockedByCompany });
   const domainsRecord = Object.fromEntries(
-    Array.from(allCompanies).map((company) => [
-      company,
-      {
-        pages: Array.from(pagesByCompany.get(company) ?? []),
-        allowed: Array.from(allowedByCompany.get(company) ?? []),
-        blocked: Array.from(blockedByCompany.get(company) ?? []),
-      },
-    ]),
+    Array.from(allCompanies).map((company) => {
+      function getArray(set: Map<CompanyTypes, Set<string>>) {
+        return Array.from(set.get(company) ?? []);
+      }
+      function withResourceType(key: string) {
+        return `${key} [${getArray(resourceTypesByCompany.get(key)!).sort()}]`;
+      }
+      return [
+        company,
+        {
+          pages: getArray(pagesByCompany),
+          allowed: getArray(allowedByCompany).map(withResourceType),
+          blocked: getArray(blockedByCompany).map(withResourceType),
+        },
+      ];
+    }),
   );
   const infraDomains = Array.from(domainsFromNode);
   return { ...domainsRecord, infra: infraDomains };
