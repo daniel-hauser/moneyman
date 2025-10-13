c8be7c4a3eb4747089fde4b96d8f720de66ac64e
Daniel Hauser <dd.hauser@gmail.com>
2025-10-13 22:44:03 +0300

    fix(scrape): retry failed scrapes

--- src/scraper/scrape.ts
+++ src/scraper/scrape.ts
@@ -7,6 +7,7 @@ import { AccountConfig } from "../types.js";
 import { ScraperErrorTypes } from "israeli-bank-scrapers/lib/scrapers/errors.js";
 import { createLogger } from "../utils/logger.js";
 import { prepareAccountCredentials } from "./otp.js";
+import { editMessage, sendError } from "../bot/notifier.js";
 
 const logger = createLogger("scrape");
 
@@ -24,15 +25,47 @@ export async function getAccountTransactions(
       onProgress(companyId, type);
     });
 
+    const accountCredentials = prepareAccountCredentials(account);
     const result = await scraper.scrape({
       ...account,
-      ...prepareAccountCredentials(account),
+      ...accountCredentials,
     });
 
-    if (!result.success) {
+    if (result.success) {
+      logger(`success: ${account.companyId}`);
+    } else if (
+      [
+        ScraperErrorTypes.ChangePassword,
+        ScraperErrorTypes.InvalidPassword,
+        ScraperErrorTypes.AccountBlocked,
+      ].includes(result.errorType!)
+    ) {
       logger(`error: ${result.errorType} ${result.errorMessage}`);
+    } else {
+      logger(`error: ${result.errorType} ${result.errorMessage}`);
+
+      const messageText = `Error scraping account ${account.companyId}: ${result.errorType} ${result.errorMessage}`;
+      const [message, retryResult] = await Promise.all([
+        sendError(`${messageText}\nretrying...`),
+        scraper.scrape({ ...account, ...accountCredentials }),
+      ]);
+
+      if (!retryResult.success) {
+        const error = `${retryResult.errorType} ${retryResult.errorMessage}`;
+        logger(`retry error: ${error}`);
+        await editMessage(
+          message?.message_id,
+          `${messageText}\nretry failed: ${error}`,
+        );
+      } else {
+        logger(`retry succeeded: ${account.companyId}`);
+        await editMessage(
+          message?.message_id,
+          `${messageText}\nretry succeeded`,
+        );
+        return retryResult;
+      }
     }
-    logger(`ended`);
 
     return result;
   } catch (e) {
@@ -42,5 +75,7 @@ export async function getAccountTransactions(
       errorType: ScraperErrorTypes.Generic,
       errorMessage: String(e),
     };
+  } finally {
+    logger(`ended`);
   }
 }
